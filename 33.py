c380ad18dbbbccacf515ddb21cb7ea9a083bce00
mac <mac@localhost.local>
2016-10-20 05:49:44 +0800

    day-02,create database

---
+++ 33.py
@@ -0,0 +1,23 @@
+import time, uuid, functools, threading, logging
+# def next_id(t = None):
+
+#     if t is None:
+#         t = time.time()
+#     return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+# print uuid.uuid4().hex
+# print next_id()
+# print id(next_id())
+# print hex(id(next_id()))
+
+def create_engine(user, password, database, host='127.0.0.1', port = 3306, **kw):
+#    import mysql.connector
+
+    paramas = dict(user=user, password = password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8', collation='utf-8_general_ci', autocommit=False)
+    for k, v in defaults.iteritems():
+        paramas[k] = kw.pop(k, v)
+        print paramas[k]
+    paramas.update(kw)
+    print paramas
+
+create_engine('comon','gloryn','mysql')
